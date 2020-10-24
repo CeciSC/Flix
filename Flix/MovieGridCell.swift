2a126bbd12155ba8ea9617cd5fda2f3148e760be
CeciSC <ceciliachen233@gmail.com>
2020-10-24 13:04:52 -0700

    part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Cecilia Chen on 10/24/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
